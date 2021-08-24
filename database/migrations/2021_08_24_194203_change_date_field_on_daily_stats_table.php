3a716d1fe2753a7aca25f02e3d16867e2d194d9d
Jake B <jake.booher@gmail.com>
2021-08-24 15:40:45 -0500

    Update 2021_08_24_194203_change_date_field_on_daily_stats_table.php

--- database/migrations/2021_08_24_194203_change_date_field_on_daily_stats_table.php
+++ database/migrations/2021_08_24_194203_change_date_field_on_daily_stats_table.php
@@ -16,7 +16,7 @@ class ChangeDateFieldOnDailyStatsTable extends Migration
         DB::table('daily_stats')->truncate();
 
         Schema::table('daily_stats', function (Blueprint $table) {
-            $table->date('date')->nullable()->change();
+            $table->date('date')->nullable()->default(null)->change();
         });
     }
 
